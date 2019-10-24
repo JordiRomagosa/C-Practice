a391b20ab6908e57179ebf9a7bb6807d0eadd2a0
Jordi <jordi@shiraz.es>
2019-10-24 19:03:05 +0200

    Project uploaded. Render 2D triangle in space

---
+++ Engine_Master/ModuleWindow.h
@@ -0,0 +1,35 @@
+#ifndef __ModuleWindow_H__
+#define __ModuleWindow_H__
+
+#include "Module.h"
+#include "SDL/include/SDL.h"
+
+class Application;
+
+class ModuleWindow : public Module
+{
+public:
+
+	ModuleWindow();
+
+	// Destructor
+	virtual ~ModuleWindow();
+
+	// Called before quitting
+	bool Init();
+
+	// Called before quitting
+	bool CleanUp();
+
+public:
+	//The window we'll be rendering to
+	SDL_Window* window = NULL;
+
+	//The surface contained by the window
+	SDL_Surface* screen_surface = NULL;
+
+	//The OpenGL context associated with the window
+	SDL_GLContext glcontext = NULL;
+};
+
+#endif // __ModuleWindow_H__
