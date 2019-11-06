842c83605f98fc703c63bef169c1f616b335a6a8
Jordi <jordi@shiraz.es>
2019-11-06 20:34:14 +0100

    Grid shown

--- Engine_Master/ModuleRenderExercise.cpp
+++ Engine_Master/ModuleRenderExercise.cpp
@@ -44,9 +44,9 @@ bool ModuleRenderExercise::Init()
 	glEnable(GL_TEXTURE_2D);
 	glViewport(0, 0, 1024, 768);
 
-	eye = float3(0, 0, 3);
+	eye = float3(0, 1, 3);
 	up = float3(0, 1, 0);
-	target = float3(0, 0, 0);
+	target = float3(0, 1, 0);
 
 	float buffer_data[] = { -1.0f, -1.0f, 0.0f,
 							1.0f, -1.0f, 0.0f,
@@ -88,6 +88,41 @@ update_status ModuleRenderExercise::Update()
 	glViewport(0, 0, w, h);
 
 	float aspect = float(w) / float(h);
+
+	glLineWidth(1.0f);
+	float d = 200.0f;
+	glBegin(GL_LINES);
+	
+	for (float i = -d; i <= d; i += 1.0f)
+	{
+		glVertex3f(i, 0.0f, -d);
+		glVertex3f(i, 0.0f, d);
+		glVertex3f(-d, 0.0f, i);
+		glVertex3f(d, 0.0f, i);
+	}
+	glEnd();
+
+	glLineWidth(2.0f);
+	glBegin(GL_LINES);
+	// red X
+	glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
+	glVertex3f(0.0f, 0.0f, 0.0f); glVertex3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(1.0f, 0.1f, 0.0f); glVertex3f(1.1f, -0.1f, 0.0f);
+	glVertex3f(1.1f, 0.1f, 0.0f); glVertex3f(1.0f, -0.1f, 0.0f);
+	// green Y
+	glColor4f(0.0f, 1.0f, 0.0f, 1.0f);
+	glVertex3f(0.0f, 0.0f, 0.0f); glVertex3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(-0.05f, 1.25f, 0.0f); glVertex3f(0.0f, 1.15f, 0.0f);
+	glVertex3f(0.05f, 1.25f, 0.0f); glVertex3f(0.0f, 1.15f, 0.0f);
+	glVertex3f(0.0f, 1.15f, 0.0f); glVertex3f(0.0f, 1.05f, 0.0f);
+	// blue Z
+	glColor4f(0.0f, 0.0f, 1.0f, 1.0f);
+	glVertex3f(0.0f, 0.0f, 0.0f); glVertex3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(-0.05f, 0.1f, 1.05f); glVertex3f(0.05f, 0.1f, 1.05f);
+	glVertex3f(0.05f, 0.1f, 1.05f); glVertex3f(-0.05f, -0.1f, 1.05f);
+	glVertex3f(-0.05f, -0.1f, 1.05f); glVertex3f(0.05f, -0.1f, 1.05f);
+	glEnd();
+	glLineWidth(1.0f);
 
 	Frustum frustum;
 	frustum.type = FrustumType::PerspectiveFrustum;
